6987c92ad31d2bd2f7d75d45317150ee593c678f
omnisns <znwhaner@163.com>
2019-12-13 20:30:54 +0800

    初始化

---
+++ src/main/java/com/synchea/slapface/SlapfaceApplication.java
@@ -0,0 +1,13 @@
+package com.synchea.slapface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlapfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlapfaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/synchea/slapface/SlapfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synchea.slapface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlapfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
